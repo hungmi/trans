3dd2ffc0b9c37265d865bfad11aef0493c01032e
hungmi <hungmihungmi@gmail.com>
2018-07-06 00:30:50 +0800

    fix autoplay issue

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,73 +1,105 @@
 document.addEventListener('turbolinks:load', () => {
+    // 首頁影片
+    videoContent = document.getElementById('videoBg');
+    checkAutoplaySupport();
+
     /// 讓頁面跳轉接過場柔順一點
     $('body').removeClass('hide')
-    $('#nav').on('click', 'a', function() {
-        $('body').addClass('hide')
-    });
-
-    /// 載入 partial 們
-    // $('#nav').load('../../partail/_nav.html');
-    // $('#header').load('../partail/_header.html');
-    // $('#footer').load('../partail/_footer.html');
-
-    $('#header').on('click', '#mobile-nav', function() {
-        $('.mobile-nav-btn').toggleClass('active');
-        $('#mobile-nav-list').toggleClass('active');
-    });
-
-    //FAQ展開的部分
-    $(".faq-q").click(function() {
-        var container = $(this).parents(".faq-c");
-        var answer = container.find(".faq-a");
-        var trigger = container.find(".faq-t");
-
-        answer.slideToggle(200);
-
-        if (trigger.hasClass("faq-o")) {
-            trigger.removeClass("faq-o");
-        } else {
-            trigger.addClass("faq-o");
-        }
-    });
-
-    // CommunityPartner
-    // var $partnerLink = $('#partner_link');
-    // if ($partnerLink.length > 0) {
-    //     $(window).bind('scroll', function() {
-    //         var whereAmI = $(this).scrollTop(),
-    //             target = $('.main_title').offset().top;
-            
-    //         if (whereAmI < target) {
-    //             $('#partner_link').removeClass('fixed')
-    //         } else if (whereAmI > target) {
-    //             $('#partner_link').addClass('fixed')
-    //         }
-    //     }).scroll();
-    // }
-
+    
     //議程的tab
     $('.tabgroup > div').hide();
     $('.tabgroup > div:first-of-type').show();
-    $('.tabs a').click(function(e) {
-        e.preventDefault();
-        var $this = $(this),
-            tabgroup = '#' + $this.parents('.tabs').data('tabgroup'),
-            others = $this.closest('li').siblings().children('a'),
-            target = $this.attr('href');
-
-        others.removeClass('active');
-        $this.addClass('active');
-        $(tabgroup).children('div').hide();
-        $(target).show();
-
-    });
 });
 
-// new Vue({
-//  el: '#shift'
-//  data: {
-//      videos: [],
-//      photos: [],
-//      files: []
-//  }
-// })
+$(document).on('click', '#nav a', function() {
+    $('body').addClass('hide')
+});
+
+$(document).on('click', '#header #mobile-nav', function() {
+    $('.mobile-nav-btn').toggleClass('active');
+    $('#mobile-nav-list').toggleClass('active');
+});
+
+//FAQ展開的部分
+$(document).on('click', '.faq-q', function() {
+    var container = $(this).parents(".faq-c");
+    var answer = container.find(".faq-a");
+    var trigger = container.find(".faq-t");
+
+    answer.slideToggle(200);
+
+    if (trigger.hasClass("faq-o")) {
+        trigger.removeClass("faq-o");
+    } else {
+        trigger.addClass("faq-o");
+    }
+});
+
+//議程的tab
+$(document).on('click', '.tabs a', function(e) {
+    e.preventDefault();
+    var $this = $(this),
+        tabgroup = '#' + $this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('li').siblings().children('a'),
+        target = $this.attr('href');
+
+    others.removeClass('active');
+    $this.addClass('active');
+    $(tabgroup).children('div').hide();
+    $(target).show();
+
+});
+
+function checkAutoplaySupport() {
+  var playPromise = videoContent.play();
+  if (playPromise !== undefined) {
+    playPromise.then(onAutoplayWithSoundSuccess).catch(onAutoplayWithSoundFail);
+  }
+}
+
+function onAutoplayWithSoundSuccess() {
+  console.log("If we make it here, unmuted autoplay works.")
+  videoContent.pause();
+  autoplayAllowed = true;
+  autoplayRequiresMuted = false;
+  autoplayChecksResolved();
+}
+
+function onAutoplayWithSoundFail() {
+  console.log("Unmuted autoplay failed. Now try muted autoplay.")
+  checkMutedAutoplaySupport();
+}
+
+function checkMutedAutoplaySupport() {
+  videoContent.volume = 0;
+  videoContent.muted = true;
+  var playPromise = videoContent.play();
+  if (playPromise !== undefined) {
+    playPromise.then(onMutedAutoplaySuccess).catch(onMutedAutoplayFail);
+  }
+}
+
+function onMutedAutoplaySuccess() {
+  console.log("If we make it here, muted autoplay works but unmuted autoplay does not.")
+  videoContent.pause();
+  autoplayAllowed = true;
+  autoplayRequiresMuted = true;
+  autoplayChecksResolved();
+}
+
+function onMutedAutoplayFail() {
+  console.log("Both muted and unmuted autoplay failed. Fall back to click to play.")
+  videoContent.volume = 1;
+  videoContent.muted = false;
+  autoplayAllowed = false;
+  autoplayRequiresMuted = false;
+  autoplayChecksResolved();
+}
+
+function autoplayChecksResolved() {
+  if (autoplayAllowed) {
+    videoContent.play();
+  } else {
+    videoContent.controls = true;
+  }
+}
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @show_footer = false
   end
 
   def about
