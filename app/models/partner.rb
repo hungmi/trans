5fa7ec87e095c1076a56766ab4b7126713c79d5c
hungmi <hungmihungmi@gmail.com>
2018-07-11 00:13:19 +0800

    validates Partner.link

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,3 +1,4 @@
 class Partner < ApplicationRecord
 	has_one_attached :cover
+	validates_format_of :link, with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix, message: "請確認連結網址是否正確，並包含開頭 http:// 或 https://"
 end
