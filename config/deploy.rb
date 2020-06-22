2af8329463d90a53d08a59acd0e53902c598deca
hungmi <hungmihungmi@gmail.com>
2020-06-23 00:38:23 +0800

    edit Capfile and deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,33 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
-
 set :application, "trans"
 set :repo_url, "git@github.com:hungmi/trans.git"
-set :branch, :master
-set :deploy_to, '/home/deploy/railsapp/trans'
+
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+set :db_backup_path, "/home/deploy/#{fetch :application}/db_backups"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.0"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :false
 
 # Default value for :pty is false
-set :pty, true
-set :linked_files, %w{config/database.yml config/master.key}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads storage}
-set :bundle_binstubs, nil
-# set :rvm_type, :user
-# set :rvm_ruby_version, 'ruby-2.5.1' # Edit this if you are using MRI Ruby
-set :rbenv_type, :user # or :system, depends on your rbenv setup
-# set :rbenv_ruby, '2.5.1'
+# set :pty, true
 
-# in case you want to set ruby version from the file:
-set :rbenv_ruby, File.read('.ruby-version').strip
-# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-# set :rbenv_roles, :all # default value
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
 
@@ -41,44 +41,7 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+# set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_access.log"
-set :puma_error_log, "#{shared_path}/log/puma_error.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 16]
-set :puma_workers, 2
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, true
-
-namespace :deploy do
-  desc 'Create Directories for Puma Pids and Socket'
-    task :make_dirs do
-      on roles(:app) do
-        execute "mkdir #{shared_path}/tmp/sockets -p"
-        execute "mkdir #{shared_path}/tmp/pids -p"
-      end
-    end
-
-  desc 'Upload YAML files.'
-    task :upload_yml do
-      on roles(:app) do
-        execute "mkdir #{shared_path}/config -p"
-        upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
-        upload! StringIO.new(File.read("config/master.key")), "#{shared_path}/config/master.key"
-      end
-    end
-
-  before :starting, :upload_yml
-  before :starting, :make_dirs
-end
